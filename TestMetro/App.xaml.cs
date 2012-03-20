7ab390b528f6e362a07c1475299145c572b126fa
Taylor F <tmf.000@T-PC.(none)>
2012-03-19 22:05:27 -0400

    Made metro test more useful, displays playlists

--- TestMetro/App.xaml.cs
+++ TestMetro/App.xaml.cs
@@ -23,10 +23,13 @@ namespace TestMetro
     /// </summary>
     sealed partial class App : Application
     {
+        
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
+        /// [STAThread]
+        /// 
        public App()
         {
             this.InitializeComponent();
--- TestMetro/BlankPage.xaml.cs
+++ TestMetro/BlankPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using GoogleMusicAPI;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,7 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using System.Threading;
+using Windows.UI.Core;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace TestMetro
@@ -30,7 +32,7 @@ namespace TestMetro
 
             api.OnLoginComplete += OnGMLoginComplete;
             api.OnGetPlaylistsComplete += api_OnGetPlaylistsComplete;
-            api.Login("user", "pass");
+            
         }
 
         void OnGMLoginComplete(object s, EventArgs e)
@@ -40,7 +42,12 @@ namespace TestMetro
 
         void api_OnGetPlaylistsComplete(GoogleMusicPlaylists pls)
         {
-           // do stuff
+            Dispatcher.Invoke(CoreDispatcherPriority.Normal, (x, y) => 
+            { 
+                foreach (GoogleMusicPlaylist p in pls.UserPlaylists)
+                    lbPlaylists.Items.Add(p.Title); 
+            }, this, null);
+            
         }
 
         /// <summary>
@@ -50,6 +57,11 @@ namespace TestMetro
         /// property is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            api.Login(tbEmail.Text, tbPass.Text);
         }
     }
 }
