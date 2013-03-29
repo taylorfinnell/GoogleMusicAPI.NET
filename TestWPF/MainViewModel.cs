5b902493f3a518da12ae541580a631f4d9b0b1f0
Mark Pitt <markjpitt@gmail.com>
2013-03-29 00:34:29 +0000

    Initial commit of WPF test UI

---
+++ TestWPF/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWPF
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		public string Email
+		{
+			get { return _email; }
+			set
+			{
+				if (_email != value)
+				{
+					_email = value;
+					RaisePropertyChanged("Email");
+				}
+			}
+		}
+
+		public ICommand FetchTracksCommand
+		{
+			get
+			{
+				if (_fetchTracks == null)
+				{
+					_fetchTracks = new RelayCommand(OnFetchTracks);
+				}
+				return _fetchTracks;
+			}
+		}
+
+		private void OnFetchTracks(object parameter)
+		{
+		}
+
+		private void RaisePropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+			}
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private string _email;
+		private ICommand _fetchTracks;
+	}
+}
---
+++ TestWPF/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWPF
+{
+	public class RelayCommand : ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+		private Predicate<object> _canExecute;
+		private Action<object> _execute;
+
+		public RelayCommand(Action<object> execute)
+		{
+			_execute = execute;
+		}
+
+		public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+		{
+			_canExecute = canExecute;
+			_execute = execute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			if (_canExecute != null)
+			{
+				return _canExecute(parameter);
+			}
+			return true;
+		}
+				
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+	}
+}
